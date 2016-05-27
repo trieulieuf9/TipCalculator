1bbe6d7689a6b3bc1b814bfdca4fa7806244ab7f
trieulieuf9 <trieulieuf9@gmail.com>
2016-05-27 16:45:24 +0700

    First Commit and also last commit

---
+++ TipCalculator/SettingsViewController.swift
@@ -0,0 +1,208 @@
+//
+//  SettingsViewController.swift
+//  TipCalculator
+//
+//  Created by trieulieuf9 on 5/16/16.
+//  Copyright © 2016 trieulieuf9. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var themeNameLabel: UILabel!
+    
+    @IBOutlet weak var segmentTextField1: UITextField!
+    @IBOutlet weak var segmentTextField2: UITextField!
+    @IBOutlet weak var segmentTextField3: UITextField!
+    
+    var choosenThemeLabel = UILabel()
+    var themeDropdownList = UITableView()
+    
+    var themeName = ["Dark", "Bright", "Colorful"]
+    var tempThemeName = "Dark"  // store theme name, before user press save changes
+    
+    // Vietnamese currency label
+    var VNDCurrencyLabel = UILabel()
+    var currencySwitch = UISwitch()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // set up button
+        let saveChangesButton = UIButton(frame: CGRectMake(self.view.frame.width / 2 - 75,
+                                                        self.view.frame.height - 85, 150, 30))
+        saveChangesButton.setTitle("Save Changes", forState: .Normal)
+        saveChangesButton.setTitleColor(UIColor.blueColor(), forState: .Normal)
+        saveChangesButton.addTarget(self, action: #selector(SettingsViewController.saveChangesButtonTouched), forControlEvents: .TouchUpInside)
+        
+        // add gesture recognizer to UIView
+        let onTap = UITapGestureRecognizer(target: self, action: #selector(SettingsViewController.onTap))
+        onTap.cancelsTouchesInView = false // leave room for detecting other touches too
+        self.view.addGestureRecognizer(onTap)
+        self.view.addSubview(saveChangesButton)
+        
+        setUpChoosenThemeLabel()
+        setUpVNDCurrencySetting()
+        // fill settings options with default values
+        fillValueToSettingsScreen()
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        // fill settings options with default values
+        fillValueToSettingsScreen()
+    }
+
+    func saveChangesButtonTouched(){
+        //let defaults = NSUserDefaults.standardUserDefaults()
+        //let stringValue = defaults.objectForKey("some_key_that_you_choose") as! String
+        //let intValue = defaults.integerForKey("another_key_that_you_choose")
+        let defaults = NSUserDefaults.standardUserDefaults()
+        
+        defaults.setInteger(Int(segmentTextField1.text!)!, forKey: "segment1")
+        defaults.setInteger(Int(segmentTextField2.text!)!, forKey: "segment2")
+        defaults.setInteger(Int(segmentTextField3.text!)!, forKey: "segment3")
+        
+        defaults.setObject(tempThemeName, forKey: "theme")
+        
+        defaults.setBool(currencySwitch.on, forKey: "vnd_currency")
+        
+        defaults.setBool(true, forKey: "is_setting_changed")
+        
+        defaults.synchronize()
+        smallToast()
+    }
+    
+    func fillValueToSettingsScreen(){
+        let defaults = NSUserDefaults.standardUserDefaults()
+        
+        // it will always be true after the first time user Save Settings
+        // if there is no value stored. return false
+        let isSettingsChanged = defaults.boolForKey("is_setting_changed")
+        
+        if (isSettingsChanged){
+            let segment1 = defaults.integerForKey("segment1")
+            let segment2 = defaults.integerForKey("segment2")
+            let segment3 = defaults.integerForKey("segment3")
+            
+            let theme = defaults.objectForKey("theme") as! String
+            let onOff = defaults.boolForKey("vnd_currency")
+            
+            segmentTextField1.text = String(segment1)
+            segmentTextField2.text = String(segment2)
+            segmentTextField3.text = String(segment3)
+            
+            choosenThemeLabel.text = theme
+            currencySwitch.setOn(onOff, animated: true)
+        }else{  // initial settings
+            segmentTextField1.text = "18"
+            segmentTextField2.text = "20"
+            segmentTextField3.text = "22"
+            
+            choosenThemeLabel.text = "Colorful"
+            currencySwitch.setOn(false, animated: true)
+        }
+    }
+    
+    func smallToast(){
+        let toastLabel = UILabel(frame: CGRectMake(self.view.frame.width/2 - 30,
+            self.view.frame.height / 2, 60, 40))
+        toastLabel.backgroundColor = UIColor.blackColor()
+        toastLabel.textColor = UIColor.whiteColor()
+        toastLabel.textAlignment = NSTextAlignment.Center;
+        self.view.addSubview(toastLabel)
+        toastLabel.text = "Saved"
+        toastLabel.alpha = 1.0
+        toastLabel.layer.cornerRadius = 10;
+        toastLabel.clipsToBounds  =  true
+        
+        let delay = 3 * Double(NSEC_PER_SEC)  // nanoseconds per seconds
+        let dispatchTime = dispatch_time(DISPATCH_TIME_NOW, Int64(delay))
+        
+        dispatch_after(dispatchTime, dispatch_get_main_queue(), {
+            
+            toastLabel.alpha = 0
+        })
+    }
+    
+    // user click everywhere on the screen to get rid of numpad
+    func onTap(){
+        self.view.endEditing(true)
+    }
+    
+    func onTap1(){
+        setUpThemeDropdownList()
+    }
+    
+    func setUpChoosenThemeLabel(){
+        choosenThemeLabel.frame = CGRectMake(self.view.frame.width - 100, 184, 80, 30);
+        choosenThemeLabel.textColor = UIColor.blueColor()
+        
+        let onTap1 = UITapGestureRecognizer(target: self, action: #selector(SettingsViewController.onTap1))
+        choosenThemeLabel.addGestureRecognizer(onTap1)
+        choosenThemeLabel.userInteractionEnabled = true
+        
+        self.view.addSubview(choosenThemeLabel)
+    }
+    
+    func setUpThemeDropdownList(){
+        themeDropdownList.frame = CGRectMake(self.view.frame.width - 115, 212, 95, 150);
+        //themeDropdownList.layer.borderWidth = 2.0
+        
+        themeDropdownList.delegate = self
+        themeDropdownList.dataSource = self
+        themeDropdownList.registerClass(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        
+        self.view.addSubview(themeDropdownList)
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return themeName.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell:UITableViewCell = tableView.dequeueReusableCellWithIdentifier("cell")! as UITableViewCell
+        
+        cell.textLabel?.text = themeName[indexPath.row]
+        //cell.layer.borderWidth = 1.0
+        
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        if indexPath.row == 0{ // Dark
+            choosenThemeLabel.text = "Dark"
+            tempThemeName = "Dark"
+            themeDropdownList.removeFromSuperview()
+        }else if indexPath.row == 1{ // bright
+            choosenThemeLabel.text = "Bright"
+            tempThemeName = "Bright"
+            themeDropdownList.removeFromSuperview()
+        }else{ // colorful
+            choosenThemeLabel.text = "Colorful"
+            tempThemeName = "Colorful"
+            themeDropdownList.removeFromSuperview()
+        }
+    }
+    
+    func setUpVNDCurrencySetting(){
+        // label
+        VNDCurrencyLabel.frame = CGRectMake(20, 240, 120, 30);
+        VNDCurrencyLabel.text = "VND Currency"
+        self.view.addSubview(VNDCurrencyLabel)
+        
+        // switch 
+        currencySwitch.frame = CGRectMake(self.view.frame.width - 100, 240, 60, 30);
+        currencySwitch.on = false
+        currencySwitch.setOn(true, animated: false);
+        currencySwitch.addTarget(self, action: #selector(SettingsViewController.switchValueDidChange), forControlEvents: .ValueChanged);
+        self.view.addSubview(currencySwitch);
+    }
+    
+    func switchValueDidChange(sender:UISwitch){
+        print(sender.on)
+    }
+    
+    
+}
--- TipCalculator/ViewController.swift
+++ TipCalculator/ViewController.swift
@@ -6,20 +6,217 @@
 //  Copyright © 2016 trieulieuf9. All rights reserved.
 //
 
+// What to do Next
+
+// Choose from Vietnamese and English, radio button
+
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var total:Double = 0
+    @IBOutlet weak var label1: UILabel!
+    
+    @IBOutlet weak var label2: UILabel!
+    
+    @IBOutlet weak var tipControl: UISegmentedControl!
 
+    @IBOutlet weak var billField: UITextField!
+    
+    @IBOutlet weak var tipLabel: UILabel!
+    
+    @IBOutlet weak var totalLabel: UILabel!
+    
+    var tipPercentages:[Double] = []
+    
+    // Create UIComponents programmatically
+    var eachPeopleBillLabel:UILabel = UILabel()
+    
+    var slider:UISlider = UISlider()
+    
+    //currency - VND or US
+    var isVND = false  // default will be US Dollar
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        let defaults = NSUserDefaults.standardUserDefaults()
+        // it will always be true, since the first time user Save Settings
+        // if there is no value stored. return false
+        
+        let isSettingsChanged = defaults.boolForKey("is_setting_changed")
+        
+        if (isSettingsChanged){
+            let segment1 = defaults.integerForKey("segment1")
+            let segment2 = defaults.integerForKey("segment2")
+            let segment3 = defaults.integerForKey("segment3")
+            tipPercentages = [Double(segment1) / 100,
+                              Double(segment2) / 100,
+                              Double(segment3) / 100]
+            
+            tipControl.setTitle("\(segment1)%", forSegmentAtIndex: 0)
+            tipControl.setTitle("\(segment2)%", forSegmentAtIndex: 1)
+            tipControl.setTitle("\(segment3)%", forSegmentAtIndex: 2)
+            
+            let theme = defaults.objectForKey("theme") as! String
+            changeTheme(theme)
+            
+            isVND = defaults.boolForKey("vnd_currency") // return false --> USD, true --> VND
+        }else{  // initial settings
+            tipPercentages = [0.18, 0.2, 0.22]
+            isVND = false
+        }
+        
+        setUpeachPeopleBillLabel()
+        setUpSlider()
+        makeBillFieldLookBetter()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func onEditingChange(sender: AnyObject) {
+        let tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
+        
+        let billAmount = NSString(string: billField.text!).doubleValue
+        let tip = billAmount * tipPercentage
+        total = billAmount + tip
+        
+        if isVND { // VND
+            if total > 1000 {
+                var mAmount = Int(total / 1000)
+                var kAmount = Double(total % 1000)
+                
+                // get remainder
+                // ex: 99.5, get number 5 in this number
+                let kRemainder = Int((kAmount - Double(Int(kAmount))) * 10)
+                let tipAmountRemainder = Int((tip - Double(Int(tip))) * 10)
+                
+                totalLabel.text = "\(mAmount)m\(Int(kAmount))k\(kRemainder)"
+                if tip > 1000 {
+                    mAmount = Int(tip / 1000)
+                    kAmount = Double(tip % 1000)
+                    
+                    tipLabel.text = "\(mAmount)m\(Int(kAmount))k\(kRemainder)"
+                }else{
+                    tipLabel.text = "\(Int(tip))k\(tipAmountRemainder)"
+                    //tipLabel.text = String(format: "%.1fk", tip)
+                }
+            }else{
+                let tipAmountRemainder = Int((tip - Double(Int(tip))) * 10)
+                let totalAmountRemainder = Int((total - Double(Int(total))) * 10)
+                
+                tipLabel.text = "\(Int(tip))k\(tipAmountRemainder)"
+                totalLabel.text = "\(Int(total))k\(totalAmountRemainder)"
+                
+                //tipLabel.text = String(format: "%.1fk", tip)
+                //totalLabel.text = String(format: "%.1fk", total)
+            }
+            
+        }else{ // $
+            tipLabel.text = String(format: "$%.2f", tip)
+            totalLabel.text = String(format: "$%.2f", total)
+        }
+        
+        // update bill for each people
+        sliderValueDidChange(slider)
     }
-
-
+    
+    // when user tap anywhere in the screen, close the iphone numPad
+    @IBAction func onTap(sender: AnyObject) {
+        view.endEditing(true)
+    }
+    
+    // Make the billField look better
+    func makeBillFieldLookBetter(){
+        var frameRect = billField.frame;
+        frameRect.size.height = 120;
+        
+        billField.frame = frameRect;
+        billField.font = UIFont(name: "Arial", size: 85)
+        billField.textColor = UIColor(red: 0.2, green: 0.55, blue: 0.2, alpha: 1.0)
+        billField.becomeFirstResponder()
+        billField.textAlignment = .Right
+        billField.backgroundColor = UIColor(red: 0.5, green: 0.7, blue: 0.3, alpha: 1.0)
+        if isVND {
+            billField.placeholder = "VND"
+        }else{
+            billField.placeholder = "$"
+        }
+    }
+    
+    // Set Up UI Stuffs here
+    func setUpeachPeopleBillLabel(){
+        eachPeopleBillLabel.frame = CGRectMake(0,
+            self.view.frame.height - 160, self.view.frame.width, 30)
+        eachPeopleBillLabel.font = UIFont(name: "Arial", size: 28)
+        
+        eachPeopleBillLabel.textAlignment = .Center
+        //eachPeopleBillLabel.textColor = UIColor.blackColor()
+        self.view.addSubview(eachPeopleBillLabel)
+    }
+    
+    func setUpSlider(){
+        slider.frame = CGRectMake(20, self.view.frame.height - 70,
+                                  self.view.frame.width - 40 ,20)
+        slider.minimumValue = 1
+        slider.maximumValue = 20
+        slider.continuous = true
+        slider.tintColor = UIColor.redColor()
+        slider.value = 4
+        slider.addTarget(self, action: #selector(ViewController.sliderValueDidChange), forControlEvents: .ValueChanged)
+        self.view.addSubview(slider)
+    }
+    
+    func sliderValueDidChange(slider:UISlider){
+        let numberOfPeople = Int(slider.value)
+        let billForEach = Int(total) / numberOfPeople
+        if isVND {  // vnd
+            if billForEach > 1000 {
+                let mAmount = billForEach / 1000
+                let kAmount = billForEach % 1000
+                let formattedString = "\(mAmount)m\(kAmount)k"
+                eachPeopleBillLabel.text = "\(numberOfPeople) 👫: \(formattedString) For Each"
+            }else{
+                eachPeopleBillLabel.text = "\(numberOfPeople) 👫: \(billForEach)k For Each"
+            }
+        }else{  // dollar
+            eachPeopleBillLabel.text = "\(numberOfPeople) 👫: $\(billForEach) For Each"
+        }
+    }
+    
+    func changeTheme(themeName:String){
+        if themeName == "Dark"{
+            self.view.backgroundColor = UIColor.blackColor()
+            billField.backgroundColor =
+                UIColor(red: 39/255, green: 59/255, blue: 59/255, alpha: 1.0)
+            
+            changeTextColor(UIColor.grayColor())
+            eachPeopleBillLabel.textColor = UIColor.grayColor()
+            slider.tintColor = UIColor.grayColor()
+        }else if themeName == "Bright"{
+            self.view.backgroundColor = UIColor.whiteColor()
+            billField.backgroundColor =
+                UIColor(red: 235/255, green: 235/255, blue: 250/255, alpha: 1.0)
+            slider.tintColor = UIColor.redColor()
+            tipControl.tintColor = UIColor.blackColor()
+            changeTextColor(UIColor.blackColor())
+            
+        }else{  // colorful
+            self.view.backgroundColor =
+                UIColor(red: 255/255, green: 192/255, blue: 203/255, alpha: 1.0)
+            billField.backgroundColor =
+                UIColor(red: 221/255, green: 160/255, blue: 221/255, alpha: 1.0)
+            slider.tintColor = UIColor.redColor()
+            tipControl.tintColor = UIColor.blackColor()
+            changeTextColor(UIColor.blackColor())
+        }
+    }
+    
+    func changeTextColor(color:UIColor){
+        label1.textColor = color
+        label2.textColor = color
+        tipLabel.textColor = color
+        totalLabel.textColor = color
+        tipControl.tintColor = color
+    }
 }
-
